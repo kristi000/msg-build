9c1730e2c404efccc1e9dac7df172886d8cbd023
kristish000 <kristish000@gmail.com>
2023-05-29 17:04:12 +0200

    created input and button components

---
+++ client/src/components/Input.tsx
@@ -0,0 +1,13 @@
+import { DetailedHTMLProps, forwardRef, InputHTMLAttributes } from "react"
+export const Input = forwardRef<
+  HTMLInputElement,
+  DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
+>(({ className, ...rest }, ref) => {
+  return (
+    <input
+      className={`py-1 px-2 border border-gray-400 focus:border-blue-500 outline-none rounder w-full ${className}`}
+      ref={ref}
+      {...rest}
+    />
+  )
+})
